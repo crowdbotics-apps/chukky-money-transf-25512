4e229b18ac2d713abf79f16b9070c6d98c75c924
Crowdbotics <team@crowdbotics.com>
2021-04-07 13:15:06 +0000

    add package blueprint-react-native-profiles-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from "@react-navigation/native"
 import SplashScreen from "../features/SplashScreen"
 import SideMenu from "./sideMenu"
 //@BlueprintImportInsertion
+import UserProfile222548Navigator from '../features/UserProfile222548/navigator';
 
 /**
  * new navigators can be imported here
@@ -12,6 +13,7 @@ import SideMenu from "./sideMenu"
 
 const AppNavigator = {
   //@BlueprintNavigationInsertion
+UserProfile222548: { screen: UserProfile222548Navigator },
 
   /** new navigators can be added here */
   SplashScreen: {
